20a05cb388a24edb4ab5482a07300bba0762c139
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-20 16:06:23 -0500

    working search box

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ class App extends Component {
       searchField: ''
     }
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
