b2eb80404ba45090c5e8b7887a83ec87c4d65e5d
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-25 15:49:13 -0800

    modify Header.js and CounterButton.js

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -7,11 +7,18 @@ class CounterButton extends Component {
       count: 0
     }
   }
+
   shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count !== nextState.count) {
+      return true;
+    }
+    return false;
   }
 
   updateCount = () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState(state => {
+      return {count: state.count + 1}
+    })
   }
 
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import CounterButton from './CounterButton';
 
 class Header extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return false;
+  // }
+
   render() {
     console.log('Header');
     return (
       <div>
-        <CounterButton color={'red'} />
         <h1 className='f1'>RoboFriends</h1>
+        <CounterButton color={'red'} />
       </div>
     )
   }
